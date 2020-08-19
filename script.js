7c0d1d51bbf386dd8b53082cbb225d34e8e11f5e
Bradley Lund <bradjlund@gmail.com>
2020-08-19 13:50:44 +0200

    Add color transitions

---
+++ script.js
@@ -0,0 +1,26 @@
+$(function() {
+	
+	var container = $('.gradients');
+
+	function fade() {
+
+		var divs = container.children();
+		for (let i=0;i<divs.length;i++) {
+			if (divs[i].id=="everything") {
+				divs.splice(i,1)
+			}
+		}
+
+		console.log(divs)
+		$(".current").transition({opacity: 1}, 2000, 'linear', function() {
+			$('.current').removeClass('current');
+			firstDiv = divs.first();
+			firstDiv.addClass('current').css({opacity: 0});
+			firstDiv.appendTo(container);
+			fade();
+		});
+	}
+
+	fade();
+
+});
