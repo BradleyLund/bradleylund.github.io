2f663a7d07e0c8288c0c4746ca3fa5c801f84724
Bradley Lund <bradjlund@gmail.com>
2021-08-11 20:39:03 +0200

    some styling changes

--- script.js
+++ script.js
@@ -1,44 +1,38 @@
-$(function() {
-	
-	var container = $('.gradients');
-	function fade() {
+$(function () {
+  var container = $(".gradients");
+  function fade() {
+    var divs = container.children();
+    for (let i = 0; i < divs.length; i++) {
+      if (divs[i].id == "everything" || divs[i].id == "projectsDiv") {
+        divs.splice(i, 1);
+      }
+    }
 
-		var divs = container.children();
-		for (let i=0;i<divs.length;i++) {
-			if (divs[i].id=="everything"||divs[i].id=="projectsDiv") {
-				divs.splice(i,1)
-			}
-		}
+    $(".current").transition({ opacity: 1 }, 7500, "linear", function () {
+      $(".current").removeClass("current");
+      firstDiv = divs.first();
+      firstDiv.addClass("current").css({ opacity: 0 });
+      firstDiv.appendTo(container);
+      fade();
+    });
+  }
 
-		$(".current").transition({opacity: 1}, 2000, 'linear', function() {
-			$('.current').removeClass('current');
-			firstDiv = divs.first();
-			firstDiv.addClass('current').css({opacity: 0});
-			firstDiv.appendTo(container);
-			fade();
-		});
-	}
+  // var container2 = $('.headings');
+  // console.log(container2)
 
-	
+  // function fadeText() {
 
-	// var container2 = $('.headings');
-	// console.log(container2)
+  // 	var headings = container2.children();
+  // 	console.log(headings)
 
-
-	// function fadeText() {
-
-	// 	var headings = container2.children();
-	// 	console.log(headings)
-
-	// 	$(".current").transition({opacity: 1}, 2000, 'linear', function() {
-	// 		$('.current').removeClass('current');
-	// 		firstDiv = headings.first();
-	// 		firstDiv.addClass('current').css({opacity: 0});
-	// 		firstDiv.appendTo(container2);
-	// 		fadeText();
-	// 	});
-	// }
-	fade();
-	// fadeText();
-
+  // 	$(".current").transition({opacity: 1}, 2000, 'linear', function() {
+  // 		$('.current').removeClass('current');
+  // 		firstDiv = headings.first();
+  // 		firstDiv.addClass('current').css({opacity: 0});
+  // 		firstDiv.appendTo(container2);
+  // 		fadeText();
+  // 	});
+  // }
+  fade();
+  // fadeText();
 });
